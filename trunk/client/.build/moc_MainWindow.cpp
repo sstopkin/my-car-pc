865c139149beede63cc505cea71f628a16c6209d
stopkin.sergey@gmail.com <stopkin.sergey@gmail.com@206ed1b3-2f33-b74f-7613-061d70b6398e>
2012-01-19 14:36:37 +0000

    rewrite ini configuration file support

--- trunk/client/.build/moc_MainWindow.cpp
+++ trunk/client/.build/moc_MainWindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Thu 19. Jan 08:25:46 2012
+** Created: Thu 19. Jan 21:34:49 2012
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.0)
 **
 ** WARNING! All changes made in this file will be lost!
--- trunk/client/main.cpp
+++ trunk/client/main.cpp
@@ -35,9 +35,6 @@ int main(int argc, char *argv[])
 		
     MainWindow gui(configFile);
     gui.show();
-//    VideoWindow viewer(configFile);//configFile
-//    viewer.show();
-
 	
 	return app.exec();
 }
