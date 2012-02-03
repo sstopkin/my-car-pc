c7aa11ebf403e7bf349fde55c910ce43c76ed41e
stopkin.sergey@gmail.com <stopkin.sergey@gmail.com@206ed1b3-2f33-b74f-7613-061d70b6398e>
2012-02-03 18:00:27 +0000

    ensuring compatibility with the Arduino (1 of ...)

--- client/net.cpp
+++ client/net.cpp
@@ -21,12 +21,48 @@ void net::disconn(){
     socket->disconnectFromHost();
     socket->abort();
 }
-
+/*
+F - т€говый двигатель, движение вперЄд
+R - т€говый двигатель, движение назад
+B - т€говый двигатель, торможение
+S - рулевое управление
+*/
 void net::sendData(int povState,int buttonState[30]){
     QString strSend;
-    strSend+="p"+QString::number(povState)+"b";
+
+    /*strSend+="p"+QString::number(povState)+"b";
     for(int i=0;i<30;i++){
         strSend+=QString::number(buttonState[i]);
+    }
+    strSend+="\n";
+    */
+    switch (povState) {
+    case 1 ://U
+        strSend+="F999S500";
+        break;
+    case 2 ://R
+        strSend+="B000S999";
+        break;
+    case 4 ://D
+        strSend+="R999S500";
+        break;
+    case 8 ://L
+        strSend+="B000S000";
+        break;
+    case 3 ://UR
+        strSend+="F999S999";
+        break;
+    case 6 ://RD
+        strSend+="R999S999";
+        break;
+    case 12 ://LD
+        strSend+="R999S000";
+        break;//LU
+    case 9 :
+        strSend+="F999S000";
+        break;
+    default :
+        strSend+="B000S500";
     }
     strSend+="\n";
     sprintf(data, strSend.toAscii(),qPrintable(host));
